929d9be1f89193fbad927e70afee1cb34611e02d
Debian Server <srvhost@instantmorse.codes>
2023-11-20 18:22:19 +0000

    Integrated static server for frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.production', '.env.development'],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'instantmorse-frontend', 'app-frontend', 'dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
