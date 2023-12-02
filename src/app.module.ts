6150f24d10a9654249dd1f3f959355d90f3d0125
Luke Aschenbrenner <ashbren21@gmail.com>
2023-12-02 17:42:18 -0600

    Added easy local hosting boolean in the .env.production file for testing on local machine

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.production', '.env.development'],
+      isGlobal: true,
+      load: [configuration]
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'instantmorse-frontend', 'app-frontend', 'dist'),
