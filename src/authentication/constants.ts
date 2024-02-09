1d5b095ec27b8783addf713d0ebf11e26b58f172
Debian Server <srvhost@instantmorse.codes>
2024-02-09 23:13:18 +0000

    Revert "Adding pushed files to the server"
    This reverts commit 3b9d246064a59fcc3d2d57aa45238178d8845761.

--- src/authentication/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-    secret: 'DMPG123456789.',
-};
--- src/authentication/dto/create-authentication.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateAuthenticationDto {
-    userID: number;
-    username: string;
-    password: string;
-}
