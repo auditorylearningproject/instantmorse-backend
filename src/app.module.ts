dbe58184a9466ee3f36b853f09b9fbaa0be03285
DavidG <dgarst2001@gmail.com>
2024-02-14 22:34:42 -0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TranscribeService } from './transcript.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { join } from 'path';
 import configuration from './config/configuration';
 import * as config from './config/sensitive';
 import { TranscriptController } from './transcript.controller';
@@ -16,6 +16,8 @@ import * as path from 'path';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { AuthenticationController } from '../authentication/authentication.controller';
 import { UsersModule } from '../authentication/users/users.module';
+import { jwtConstants } from 'authentication/constants';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -23,28 +25,44 @@ import { UsersModule } from '../authentication/users/users.module';
       envFilePath: ['.env.production', '.env.development'],
       isGlobal: true,
       load: [configuration],
-          }),
+    }),
     ServeStaticModule.forRoot({
-      rootPath: path.resolve('/home/srvhost/project/instantmorse-frontend/app-frontend/dist'),
+      rootPath: path.resolve(
+        '/home/srvhost/project/instantmorse-frontend/app-frontend/dist',
+      ),
       serveStaticOptions: { fallthrough: true },
     }),
     //MongooseModule.forRoot('mongodb+srv://'+config.user+':'+config.pass+'@auditorylearningproject.xixsty6.mongodb.net/morse?retryWrites=true&w=majority'),
     HttpModule,
-    UsersModule
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   controllers: [AppController, TranscriptController, AuthenticationController],
-  providers: [AppService, AuthenticationService, TranscribeService, {
-    provide: APP_FILTER,
-    useClass: NotFoundFilter,
-  },],
+  providers: [
+    AppService,
+    AuthenticationService,
+    AuthenticationController,
+    TranscribeService,
+    {
+      provide: APP_FILTER,
+      useClass: NotFoundFilter,
+    },
+  ],
+  exports: [AuthenticationService],
 })
 export class AppModule {}
 
-console.error(path.resolve(
-        __dirname,
-        '..',
-        '..',
-        'instantmorse-frontend',
-        'app-frontend',
-        'dist',
-      ))
+console.error(
+  path.resolve(
+    __dirname,
+    '..',
+    '..',
+    'instantmorse-frontend',
+    'app-frontend',
+    'dist',
+  ),
+);
