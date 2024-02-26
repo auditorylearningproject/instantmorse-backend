bd6ab713ca84bd61a7a2af3fc09e7de1f6e8d4a6
Debian Server <srvhost@instantmorse.codes>
2024-02-26 23:26:52 +0000

    changes made to main branch, not in develop

--- src/authentication/users/users.schema.ts
+++ src/authentication/users/users.schema.ts
@@ -16,19 +16,31 @@ import { Document, HydratedDocument } from 'mongoose';
 //   access_token: string;
 // }
 
-export type UserDocument = HydratedDocument<User>;
+// export type UserDocument = HydratedDocument<User>;
 
-@Schema()
-export class User extends Document {
-  @Prop({ required: true })
-  userId: number;
-  @Prop({ required: true })
-  username: string;
-  @Prop({ required: true })
-  password: string;
-  @Prop()
-  access_token: string;
-  static username: string;
-}
+// @Schema()
+// export class User extends Document {
+//   @Prop({ required: true })
+//   userId: number;
+//   @Prop({ required: true })
+//   username: string;
+//   @Prop({ required: true })
+//   password: string;
+//   @Prop()
+//   access_token: string;
+//   // static username: string;
+// }
+
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  userId: Number,
+  username: String,
+  password: String,
+  access_token: String
+});
+
+const User = mongoose.model('User', userSchema);
+
 
 export const UserSchema = SchemaFactory.createForClass(User);
