fc72bc34cdc13853e81f55bc8b7f6b6fbd02d7c5
DavidG <dgarst2001@gmail.com>
2024-03-17 23:08:33 -0500

    new method for calling lessons

--- src/selectLesson/lessons/lesson.service.ts
+++ src/selectLesson/lessons/lesson.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Lesson } from './lesson.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class LessonService {
-  findAll():
-    | import('./lesson.schema').Lesson[]
-    | PromiseLike<import('./lesson.schema').Lesson[]> {
-    throw new Error('Method not implemented.');
+  constructor(
+    @InjectModel(Lesson.name, 'data') private lessonModel: Model<Lesson>, //adding the database name here fixed the issues with injection!
+  ) {}
+
+  async findAll(): Promise<Lesson[]> {
+    return this.lessonModel.find().exec();
   }
 }
